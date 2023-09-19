a6d180ffa5c55a13042666fb362799f936242f83
benroz3 <benroz35@gmail.com>
2023-09-19 15:21:13 +0300

    fixed user global state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-'use client'
+"use client";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
 import PageTransition from "@/components/PageTransition";
+import { setIsAuthUser, setUser } from "@/redux/slices/userSlice";
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (Cookies.get("token") !== undefined) {
+      dispatch(setIsAuthUser(Cookies.get("token") !== undefined));
+      dispatch(setUser(JSON.parse(localStorage.getItem("user") || "")));
+    }
+  }, [Cookies]);
+
   return (
     <PageTransition>
       <></>
--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Cookies from "js-cookie";
 import { UserStateType } from "@/utils/types";
 
 export const user = createSlice({
@@ -9,9 +8,11 @@ export const user = createSlice({
     user: null,
   } as UserStateType,
   reducers: {
+    setIsAuthUser: (state, action) => {
+      state.isAuthUser = action.payload;
+    },
     setUser: (state, action) => {
       state.user = action.payload;
-      state.isAuthUser = Cookies.get("token") !== undefined;
     },
     removeUser: (state) => {
       state.user = null;
@@ -20,5 +21,5 @@ export const user = createSlice({
   },
 });
 
-export const { setUser, removeUser } = user.actions;
+export const { setUser, removeUser, setIsAuthUser } = user.actions;
 export default user.reducer;
